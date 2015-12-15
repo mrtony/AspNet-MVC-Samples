b1c853cb56d7118e90e6cd83f813a08769a3160c
Tony Chen <tonychenn@gmail.com>
2015-12-15 08:41:17 +0800

    加入資料驗證範例程式

---
+++ MVC4_DavaValidation/MVC4_DavaValidation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC4_DavaValidation.MvcApplication" Language="C#" %>
---
+++ MVC4_DavaValidation/MVC4_DavaValidation/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4_DavaValidation.Models
+{
+    public class User
+    {
+        [DataType(DataType.Text)]
+        [StringLength(30, MinimumLength = 6)]
+        [Required]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [StringLength(255, MinimumLength = 8)]
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [StringLength(255, MinimumLength = 8)]
+        [Required]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [StringLength(128)]
+        [Required]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression("^[0-9]{8}$")]
+        [StringLength(32)]
+        public string Phone { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [StringLength(255)]
+        public string Remarks { get; set; }
+    }
+}
